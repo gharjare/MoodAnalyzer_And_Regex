640326b51c4e782e779404a78a8acc40e422605b
gharjare <gharjarechetan@gmail.com>
2022-11-08 17:59:09 +0530

    Add null

---
+++ MoodIsNullException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthmaticProblem
+{
+    public class MoodIsNullException:Exception
+    {
+        public MoodIsNullException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
