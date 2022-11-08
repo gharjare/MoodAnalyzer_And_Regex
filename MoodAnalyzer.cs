409dad3f7fb0c8e98c17daa6a2a8339ae590ae9e
gharjare <gharjarechetan@gmail.com>
2022-11-08 17:58:07 +0530

    Add moodanalyser

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthmaticProblem
+{
+    public class MoodAnalyzer
+    {
+        public string AnalysedMood(string massege)
+        {
+            if(massege.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
