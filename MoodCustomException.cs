28a2c485fd7457c476234357e4b13616067162da
gharjare <gharjarechetan@gmail.com>
2022-11-08 18:01:14 +0530

    Add customexception

---
+++ MoodCustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthmaticProblem
+{
+    public class MoodCustomException:Exception
+    {
+        public enum ExceptionType
+        {
+            Null_Message, Empty_Message
+        }
+        private readonly Exception type;
+
+        public MoodCustomException(Exception Type, string message):base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
